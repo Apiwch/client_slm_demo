bd9e6ce8be166ba5433a7c1cd71d8aa98ab1f340
Apiwch <129593511+Apiwch@users.noreply.github.com>
2024-01-23 14:15:38 +0700

    Update AnotherComponent.jsx

--- src/components/AnotherComponent.jsx
+++ src/components/AnotherComponent.jsx
@@ -13,7 +13,7 @@ function AnotherComponent({ selectedDevices, messages }) {
     const filteredMessages = messages.filter(device => device.name === selectedDevices);
 
     const fetchData = () => {
-        axios.get(`http://localhost:5000/data/test_SLM_MQTT_${selectedDevices}`)
+        axios.get(`http://192.168.1.44:5000/data/test_SLM_MQTT_${selectedDevices}`)
             .then(response => {
                 setData(response.data);
 
